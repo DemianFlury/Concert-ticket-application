f219435348d9dd4bfa230442bdc50333574aee76
Demian Flury <74318556+DemianFlury@users.noreply.github.com>
2022-07-07 11:46:11 +0200

    added some validation

--- app/Controllers/SalesController.php
+++ app/Controllers/SalesController.php
@@ -1,17 +1,52 @@
 <?php
-class SalesController{
-    public function overview(){
-
+class SalesController
+{
+    public function overview()
+    {
     }
-    public function edit(){
+    public function edit()
+    {
         require 'app/Views/mutate-sale.view.php';
     }
-    public function newTicket(){
+    public function newTicket()
+    {
         require 'app/Views/new-sale.view.php';
     }
-}
+    public function validate()
+    {
+        $errors = [];
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $name = trim($_POST['name']) ?? '';
+            $email = trim($_POST['email']) ?? '';
+            $phone = trim($_POST['phone']) ?? '';
+            $concert = trim($_POST['concert']) ?? '';
+        }
 
-public function validate()
-{
-    $_POST[""]
+        if ($name === '') {
+            $errors[] = 'Bitte geben Sie einen Namen ein.';
+        }
+        if ($email === '') {
+            $errors[] = 'Bitte geben Sie eine Email ein.';
+        } elseif (!str_contains($email, '@')) {
+            $errors[] = 'Ihre E-Mail ist ungültig';
+        }
+        if ($phone === '') {
+            $errors[] = 'Bitte geben Sie eine Telefonnummer ein.';
+        }
+        if (preg_match('/a-z/', $phone)) {
+            $errors[] = 'Bitte geben Sie nur Nummern ein.';
+        }
+        if ($concert === '') {
+            $errors[] = 'Bitte wählen Sie ein Konzert aus.';
+        }
+
+        if(count($errors) !== 0){
+            foreach($errors as $error){
+                echo $error . '<br>';
+            }
+        }
+        else{
+            require 'app/Views/sales.view.php';
+        }
+    }
 }
