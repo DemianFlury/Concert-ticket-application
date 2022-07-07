2555e19672a7cdf4559aeb2a7341a3feb72a016e
Simon-Hodel <simon-hodel@gmx.ch>
2022-07-07 16:24:33 +0200

    patch
    patch
    
    Co-Authored-By: Demian Flury <74318556+DemianFlury@users.noreply.github.com>

--- app/Models/ticketModel.php
+++ app/Models/ticketModel.php
@@ -117,8 +117,8 @@ class ticketmodel
      */
     public function getall()
     {
-        $ticket = db()->query('SELECT c.customerName, c.email, c.phone, c.loyaltybonus, a.artist, t.paydate, t.paid
-        FROM ticket AS t INNER JOIN customer AS c ON ticket.customerid = customer.customerid INNER JOIN concert AS a ON ticket.concertid = concert.concertid ORDER BY ticketid;');
+        $ticket = db()->query('SELECT c.customerName, c.email, c.phone, c.loyaltybonus, a.Artist, t.paydate, t.paid
+        FROM ticket AS t INNER JOIN customer AS c ON t.customerid = c.customerid INNER JOIN concert AS a ON t.concertid = a.concertid ORDER BY ticketid;');
         return $ticket;
     }
 
