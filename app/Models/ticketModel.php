6ab91eacf2f50f1e202dd75dcc1d2ca0a7ebeb60
Simon-Hodel <simon-hodel@gmx.ch>
2022-07-08 09:36:28 +0200

    patch
    patch
    
    Co-Authored-By: Demian Flury <74318556+DemianFlury@users.noreply.github.com>

--- app/Models/ticketModel.php
+++ app/Models/ticketModel.php
@@ -143,7 +143,7 @@ class ticketmodel
     public function getTicket(int $id)
     {
         $ticket = db()->prepare('SELECT c.customerName, c.email, c.phone, t.loyaltybonus, a.artist, t.paydate, t.paid
-        FROM tickets AS t INNER JOIN customer AS c ON t.customerid = c.customerid INNER JOIN concerts AS a ON t.ConcertID = c.ConcertID where ticketid =:ticketid');
+        FROM tickets AS t INNER JOIN customer AS c ON t.customerid = c.customerid INNER JOIN concerts AS a ON t.ConcertID = a.ConcertID where ticketid =:ticketid');
         $ticket->bindParam(':ticketid', $id);
         $ticket->execute();
         $theticket = $ticket->fetch();
