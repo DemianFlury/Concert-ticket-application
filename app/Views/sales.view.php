6a8dfc4d1ada303932dace66f83452c2d615a84b
Demian Flury <74318556+DemianFlury@users.noreply.github.com>
2022-07-08 08:45:16 +0200

    overview is now lookin fine
    Co-Authored-By: Simon-Hodel <90034495+Simon-Hodel@users.noreply.github.com>

--- app/Views/sales.view.php
+++ app/Views/sales.view.php
@@ -1,14 +1,42 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Übersicht</title>
+    <style>
+        td {
+            border: black solid 1px;
+            font-size: 20px;
+        }
+    </style>
 </head>
+
 <body>
-    <?php foreach($allsales as $sale) : ?>
-        <?= $sale ?><br>
-    <?php endforeach; ?>
+    <table>
+        <tr>
+            <td>Name des Kunden</td>
+            <td>E-Mail Adresse</td>
+            <td>Telefonnummer</td>
+            <td>Treuebonus</td>
+            <td>Konzert / Künstler</td>
+            <td>Ablauf der Zahlung</td>
+            <td>Bezahlt</td>
+        </tr>
+        <?php foreach ($allsales as $sale) : ?>
+            <tr>
+                <td> <?= $sale['customerName']; ?> </td>
+                <td> <?= $sale['email']; ?> </td>
+                <td> <?= $sale['phone']; ?> </td>
+                <td> <?= $sale['loyaltybonus']; ?> </td>
+                <td> <?= $sale['Artist']; ?> </td>
+                <td> <?= $sale['paydate']; ?> </td>
+                <td> <?= $sale['paid'] ?> </td>
+            </tr>
+        <?php endforeach; ?>
+    </table>
 </body>
+
 </html>
